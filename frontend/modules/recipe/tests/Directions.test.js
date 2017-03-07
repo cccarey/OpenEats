d0f3f4f7f0d426c261c8fe8c9eda250ade23e899
ryan.noelk <ryannoelk@gmail.com>
2017-03-07 09:44:15 -0500

    Prevent filters from breaking when the page is small

--- frontend/modules/recipe/tests/Directions.test.js
+++ frontend/modules/recipe/tests/Directions.test.js
@@ -3,22 +3,31 @@ import Directions from '../components/Directions';
 import renderer from 'react-test-renderer';
 
 test('Link changes the class when hovered', () => {
-  const data = [{"id":1,"step":1,"title":"Brown the ground pork and ground sirlion in a medium pan. Add a teaspoon of sereano pepper while browning the meat. Season with kosher salt and pepper.","recipe":1},{"id":2,"step":2,"title":"Chop the onion, bell pepper and one Serrano pepper and place them in a large pot.","recipe":1},{"id":3,"step":3,"title":"Open up and drain both cans of kidney beans and add them to the large pot.","recipe":1},{"id":4,"step":4,"title":"Open up both cans of stewed chili style tomatoes and add them to the pot.","recipe":1},{"id":5,"step":5,"title":"Drain the grease away from the browned meat and add the meat to the pot.","recipe":1},{"id":6,"step":6,"title":"Pour in the tomato juice over the meat mixture.","recipe":1},{"id":7,"step":7,"title":"Add kosher salt, black pepper, two table spoons of chili powder, and two teaspoons of ground cumin. Stir well.","recipe":1},{"id":8,"step":8,"title":"Cook slowly over medium low heat for an hour. If it starts to bubble turn down the heat. Taste during the cooking process to check the seasoning add more to taste.","recipe":1}];
+  let data = [
+    {
+      "id":1,
+      "step":1,
+      "title":"Brown the ground pork and ground sirlion in a medium pan. Add a teaspoon of sereano pepper while browning the meat. Season with kosher salt and pepper.",
+      "recipe":1
+    },
+    {
+      "id":2,
+      "step":2,
+      "title":"Chop the onion, bell pepper and one Serrano pepper and place them in a large pot.",
+      "recipe":1
+    }
+  ];
   const component = renderer.create(
     <Directions data={ data }/>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
-  // // manually trigger the callback
-  // tree.props.onMouseEnter();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
-  //
-  // // manually trigger the callback
-  // tree.props.onMouseLeave();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  data = [{"id":1,"step":1,"title":"Brown the ground pork and ground sirlion in a medium pan. Add a teaspoon of sereano pepper while browning the meat. Season with kosher salt and pepper.","recipe":1},{"id":2,"step":2,"title":"Chop the onion, bell pepper and one Serrano pepper and place them in a large pot.","recipe":1},{"id":3,"step":3,"title":"Open up and drain both cans of kidney beans and add them to the large pot.","recipe":1},{"id":4,"step":4,"title":"Open up both cans of stewed chili style tomatoes and add them to the pot.","recipe":1},{"id":5,"step":5,"title":"Drain the grease away from the browned meat and add the meat to the pot.","recipe":1},{"id":6,"step":6,"title":"Pour in the tomato juice over the meat mixture.","recipe":1},{"id":7,"step":7,"title":"Add kosher salt, black pepper, two table spoons of chili powder, and two teaspoons of ground cumin. Stir well.","recipe":1}];
+
+  // manually trigger the callback
+  tree.props.data = data;
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
